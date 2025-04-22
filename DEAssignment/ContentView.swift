ca86c92871893b44cfe1cd8725045a3826ea6e7f
Sérgio <sergiogomesr@gmail.com>
2025-04-22 12:28:49 -0700

    Initial Commit

---
+++ DEAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DEAssignment
+//
+//  Created by Sérgio Ruediger on 22/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DEAssignment/DEAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEAssignmentApp.swift
+//  DEAssignment
+//
+//  Created by Sérgio Ruediger on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DEAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
